b1bc9e723706ab4935e6d55d941b7f07cc1895c4
eeesnghyun <szhyun2002@gmail.com>
2023-11-19 17:23:07 +0900

    [feature] 엑셀 파일 저장 기능 추가

--- src/main/java/com/app/onebiteword/OnebitewordApplication.java
+++ src/main/java/com/app/onebiteword/OnebitewordApplication.java
@@ -2,6 +2,18 @@ package com.app.onebiteword;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import java.nio.charset.Charset;
+import java.util.Collections;
 
 @SpringBootApplication
 public class OnebitewordApplication {
@@ -9,5 +21,8 @@ public class OnebitewordApplication {
     public static void main(String[] args) {
         SpringApplication.run(OnebitewordApplication.class, args);
     }
+
+
+
 
 }
--- src/main/java/com/app/onebiteword/controller/WordController.java
+++ src/main/java/com/app/onebiteword/controller/WordController.java
@@ -1,8 +1,12 @@
 package com.app.onebiteword.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Slf4j
 @Controller
 public class WordController {
 
@@ -11,6 +15,14 @@ public class WordController {
         return "index";
     }
 
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String upload(
+            @RequestBody String wordList
+    ) throws Exception {
+        log.info(wordList);
+
+        return "redirect:/";
+    }
 
 
 

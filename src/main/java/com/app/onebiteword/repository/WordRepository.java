f5e6ee0fa8b91b60b3034711217f2a9401bb0c0f
eeesnghyun <szhyun2002@gmail.com>
2023-11-19 15:37:04 +0900

    init

--- src/main/java/com/app/onebiteword/repository/WordRepository.java
+++
@@ -1,9 +0,0 @@
-package com.app.onebiteword.repository;
-
-import com.app.onebiteword.entity.WordInfoEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface WordRepository extends JpaRepository<WordInfoEntity, Long> {
-}
